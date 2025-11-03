7846b6d3350d35c9455547fa5df2fb102646da25
Markus Huber <git+markus@obermui.de>
2025-11-03 17:36:58 +0700

    Remove deprecated file upload routes and schemas from the API

--- backend/src/core/security.py
+++ backend/src/core/security.py
@@ -184,27 +184,3 @@ oauth.register(
     client_kwargs={'scope': 'openid email profile'},
     #redirect_uri=settings.GOOGLE_REDIRECT_URI
 )
-
-# GitHub OAuth registration
-oauth.register(
-    name='github',
-    client_id=settings.GITHUB_CLIENT_ID,
-    client_secret=settings.GITHUB_CLIENT_SECRET,
-    access_token_url='https://github.com/login/oauth/access_token',
-    authorize_url='https://github.com/login/oauth/authorize',
-    api_base_url='https://api.github.com/',
-    client_kwargs={'scope': 'read:user user:email'},
-    #redirect_uri=settings.GITHUB_REDIRECT_URI
-)
-
-# Discord OAuth registration
-oauth.register(
-    name='discord',
-    client_id=settings.DISCORD_CLIENT_ID,
-    client_secret=settings.DISCORD_CLIENT_SECRET,
-    access_token_url='https://discord.com/api/oauth2/token',
-    authorize_url='https://discord.com/api/oauth2/authorize',
-    api_base_url='https://discord.com/api/',
-    client_kwargs={'scope': 'identify email'},
-)
-
